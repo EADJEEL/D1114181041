1e6ee3f392e5243ddfbce5886f2b98bde84dcf66
D1114181041 <D1114181041@gm.lhu.edu.tw>
2024-10-08 12:04:37 +0800

    製作填寫科系性別的表格顯示在PHP 但錯誤了

--- table_gen.php
+++ table_gen.php
@@ -2,7 +2,24 @@
 
 $row_count = $_GET['row_num'];
 $col_count = $_GET['col_num'];
+$gender = $_GET['gender'];
+$dept = $_GET['dept'];
+$messger = $_GET['messger'];
 
 echo '表格大小' . $row_count . '列,' . $col_count . '行,';
+echo '性別:' . $gender .'' ;
+echo '科系:' . $dept .'' ;
+echo '留言:' . $messger .'' ;
 
+echo "table border=\"1\">";
+
+for ($i = 0; $i < $row_count; $i++) {
+    echo '<tr>';
+    for ($j = 0; $j < $col_count; $j++) 
+    {
+        echo '<td>行'. ($i+1) .'列'. ($i+1) .'</td>'
+    }
+    echo '</tr>';
+}
+echo '</table>';
 ?>
